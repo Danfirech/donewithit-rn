aa80654b84caa5821ba408d09223657480914a40
Dan Possehl <danielbpossehl@gmail.com>
2021-12-11 18:52:08 -0700

    added configration packager

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("app executed");
+
   return (
     <View style={styles.container}>
-      <Text>Hello React native</Text>
+      <Text>Hello Big</Text>
       <StatusBar style="auto" />
     </View>
   );
