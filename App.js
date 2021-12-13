2f5c08f33a3ed8b478156dde22cfa93cbdb1491f
Dan Possehl <danielbpossehl@gmail.com>
2021-12-13 10:36:32 -0700

    on to components

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  console.log("app executed");
-
   return (
-    <View style={styles.container}>
-      <Text>Hello Big</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={() => console.log("text clicked")}>
+        Hello Big- A really really really long text that shouldnt be in here
+        because it is wayyyy to long and life is long but this is longer
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "dodgerblue",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
