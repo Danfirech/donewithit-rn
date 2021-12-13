39e2ef45483607fe79d26fa37116ecfb2d10b276
Dan Possehl <danielbpossehl@gmail.com>
2021-12-13 12:00:24 -0700

    added alert

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text Pressed");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello Biggy</Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
-        <Image
-          blurRadius={2}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableNativeFeedback>
+      <Button
+        color="pink"
+        title="click me"
+        onPress={() => alert("button tapped")}
+      />
     </SafeAreaView>
   );
 }
