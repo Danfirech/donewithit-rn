a21eccbc5c122dde0fd9c8f9695fe4fe70cde2ac
Dan Possehl <danielbpossehl@gmail.com>
2021-12-13 11:19:18 -0700

    have to restart

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Text Pressed");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>
-        Hello Biggy- A really really really long text that shouldnt be in here
-        because it is wayyyy to long and life is long but this is longer
-      </Text>
-      <Image
-        blurRadius={2}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <Text onPress={handlePress}>Hello Biggy</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
+        <Image
+          blurRadius={2}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableNativeFeedback>
     </SafeAreaView>
   );
 }
