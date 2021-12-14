2f6f67bdb4c592d30a6dedc306f98602f4748aef
Dan Possehl <danielbpossehl@gmail.com>
2021-12-13 19:39:50 -0700

    starting welcome screen

--- App.js
+++ App.js
@@ -1,43 +1,41 @@
 import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Alert,
-  Button,
-  Platform,
-  StatusBar,
-  View,
-  Dimensions,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+
+import { View } from "react-native";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
-  console.log(Dimensions.get("screen"));
-
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          width: 100,
+          height: 100,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
